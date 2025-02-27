aa1933c19e16e725c3e612b8f0ea6ab0a8373129
sh0ma360215 <tousin360215@gmail.com>
2025-02-27 23:18:34 +0900

    vercel アナリティクス　追記

--- choice-ease/src/app/layout.tsx
+++ choice-ease/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+
+<Analytics/>
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
